253588407d8346e34bbfb2c7175c67bb4e84f960
Vladimir Smyshlyaev <v@vforvova.ru>
2017-08-01 17:23:43 +0300

    Bundler gem additions

---
+++ spec/pushwoosh_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Pushwoosh do
+  it "has a version number" do
+    expect(Pushwoosh::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
